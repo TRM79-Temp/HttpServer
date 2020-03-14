1cc21d44235de7d88b2ed7e27f86eb3c0d1d8966
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2020-03-14 09:41:54 +0200

    Processing OALD redirections.

--- HttpServer/Client.cs
+++ HttpServer/Client.cs
@@ -54,11 +54,11 @@ namespace HttpServer
                     break;
                 }
             }
-    
+
             // Парсим строку запроса с использованием регулярных выражений
             // При этом отсекаем все переменные GET-запроса
             Match ReqMatch = Regex.Match(Request, @"^\w+\s+([^\s\?]+)[^\s]*\s+HTTP/.*|");
-    
+
             // Если запрос не удался
             if (ReqMatch == Match.Empty)
             {
@@ -66,14 +66,14 @@ namespace HttpServer
                 SendError(Client, 400);
                 return;
             }
-    
+
             // Получаем строку запроса
             string RequestUri = ReqMatch.Groups[1].Value;
-    
+
             // Приводим ее к изначальному виду, преобразуя экранированные символы
             // Например, "%20" -> " "
             RequestUri = Uri.UnescapeDataString(RequestUri);
-    
+
             // Если в строке содержится двоеточие, передадим ошибку 400
             // Это нужно для защиты от URL типа http://example.com/../../file.txt
             if (RequestUri.IndexOf("..") >= 0)
@@ -81,28 +81,32 @@ namespace HttpServer
                 SendError(Client, 400);
                 return;
             }
-    
+
+            RequestUri = RequestUri.Substring(1);
+            Console.WriteLine(RequestUri);
+
             // Если строка запроса оканчивается на "/", то добавим к ней index.html
-            if (RequestUri.EndsWith("/"))
-            {
-                RequestUri += "index.html";
-            }
-    
-            string FilePath = "www/" + RequestUri;
-    
-            // Если в папке www не существует данного файла, посылаем ошибку 404
-            if (!File.Exists(FilePath))
-            {
-                SendError(Client, 404);
-                return;
-            }
-    
+//**            if (RequestUri.EndsWith("/"))
+//**            {
+//**                RequestUri += "index.html";
+//**            }
+//**
+//**            string FilePath = "www/" + RequestUri;
+//**
+//**            // Если в папке www не существует данного файла, посылаем ошибку 404
+//**            if (!File.Exists(FilePath))
+//**            {
+//**                SendError(Client, 404);
+//**                return;
+//**            }
+
             // Получаем расширение файла из строки запроса
-            string Extension = RequestUri.Substring(RequestUri.LastIndexOf('.'));
-    
+//**            string Extension = RequestUri.Substring(RequestUri.LastIndexOf('.'));
+            string Extension = "";
+
             // Тип содержимого
             string ContentType = "";
-    
+
             // Пытаемся определить тип содержимого по расширению файла
             switch (Extension)
             {
@@ -135,37 +139,76 @@ namespace HttpServer
                     }
                     break;
             }
-    
+
             // Открываем файл, страхуясь на случай ошибки
-            FileStream FS;
-            try
-            {
-                FS = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-            }
-            catch (Exception)
-            {
-                // Если случилась ошибка, посылаем клиенту ошибку 500
-                SendError(Client, 500);
-                return;
-            }
-    
+//**            FileStream FS;
+//**            try
+//**            {
+//**                FS = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+//**            }
+//**            catch (Exception)
+//**            {
+//**                // Если случилась ошибка, посылаем клиенту ошибку 500
+//**                SendError(Client, 500);
+//**                return;
+//**            }
+
+            WebClient client = new WebClient();
+            var data = client.DownloadData(RequestUri);
+
+            data = Crypt(data, "123");
+
             // Посылаем заголовки
-            string Headers = "HTTP/1.1 200 OK\nContent-Type: " + ContentType + "\nContent-Length: " + FS.Length + "\n\n";
+            string Headers = "HTTP/1.1 200 OK\nContent-Type: " + ContentType + "\nContent-Length: " + data.Length + "\n\n";
             byte[] HeadersBuffer = Encoding.ASCII.GetBytes(Headers);
             Client.GetStream().Write(HeadersBuffer, 0, HeadersBuffer.Length);
-    
-            // Пока не достигнут конец файла
-            while (FS.Position < FS.Length)
-            {
-                // Читаем данные из файла
-                Count = FS.Read(Buffer, 0, Buffer.Length);
-                // И передаем их клиенту
-                Client.GetStream().Write(Buffer, 0, Count);
-            }
-    
-            // Закроем файл и соединение
-            FS.Close();
+
+//**            // Пока не достигнут конец файла
+//**            while (FS.Position < FS.Length)
+//**            {
+//**                // Читаем данные из файла
+//**                Count = FS.Read(Buffer, 0, Buffer.Length);
+//**                // И передаем их клиенту
+//**                Client.GetStream().Write(Buffer, 0, Count);
+//**            }
+//**
+//**            // Закроем файл и соединение
+//**            FS.Close();
+
+            Client.GetStream().Write(data, 0, data.Length);
+
             Client.Close();
         }
+
+        static byte[] Crypt(byte[] data, string key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(key);
+            }
+
+            if (key == String.Empty)
+            {
+                return data;
+            }
+
+            var result = new byte[data.Length];
+
+            var passBytes = Encoding.UTF8.GetBytes(key);
+            var count = 0;
+
+            for (var i = 0; i < data.Length; i++)
+            {
+                result[i] = (byte)(data[i] ^ passBytes[count++]);
+
+                if (count == passBytes.Length)
+                {
+                    count = 0;
+                }
+            }
+
+            return result;
+        }
+
     }
 }
